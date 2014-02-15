480b08dc48c8660fd5c9f4eac6fb6f58488ca311
katrina <katrina@katrina-VirtualBox.(none)>
2014-02-14 16:57:40 -0800

    Almost done, 5 functions remaining

--- skills1.py
+++ skills1.py
@@ -40,15 +40,24 @@ def largest(some_list):
 
 # Write a function that takes a list of numbers and returns a new list of all those numbers divided by two.
 def halvesies(some_list):
-    return []
+    new_list = []
+    for i in some_list:
+        new_list.append(i/2)
+    return new_list
 
 # Write a function that takes a list of words and returns a list of all the lengths of those words.
 def word_lenths(word_list):
-    return []
+    lengths = []
+    for i in word_list:
+        lengths.append(len(i))
+    return lengths
 
 # Write a function (using iteration) that sums all the numbers in a list.
 def sum_numbers(numbers):
-    return []
+    total = 0
+    for i in numbers:
+        total += i
+    return total
 
 # Write a function that multiplies all the numbers in a list together.
 def mult_numbers(numbers):
@@ -62,5 +71,6 @@ def join_strings(string_list):
 def average(numbers):
     return None
 
-list = ["dog", "cat", "motorcycle", "something","none"]
-print long_words(list)
+words = ["dog", "cat", "motorcycle", "something","none"]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print sum_numbers(numbers)
