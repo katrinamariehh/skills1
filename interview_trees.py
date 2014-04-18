a3a4a4aa0019b9d6ff08070f8c601b83f11d271e
katrina <katrina.m.hall@gmail.com>
2014-04-18 11:47:18 -0700

    Binary tree search, depth-first and breadth-first

--- interview_trees.py
+++ interview_trees.py
@@ -19,3 +19,39 @@ def find(r, n):
 			r = r.left
 	return r
 
+# searching a binary tree that is not a binary search tree
+# depth-first search--you go all the way down, then come back up
+def search (r, n):
+	if not r:
+		return False
+	if r.data == n:
+		return True
+	# this
+	return search(r.left) or search(r.right)
+
+	# or this to return the data 
+	if not r:
+		return None
+	if r.data == n:
+		return r.data
+	left = search(r.left, n)
+	if left:
+		return left
+	right = search(r.right, nelse:
+	if right:
+		return search (r.left, n)
+
+# breadth-first searching
+# if we have a queue--we can put all of the things we need to search
+# in the queue, we just have to go to the end of the queue
+def search_bfs(r, n):
+	# we want a queue
+	q = [r]
+	while len(q) > 0:
+		node = q.pop(0)
+		if node.data == n:
+			return node
+		if node.left:
+			q.append(node.right)
+		if node.right:
+			q.append(node.left)
